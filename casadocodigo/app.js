6ef8431d6a8452e5cb27edb4ce55524858e68950
Milene <milenelacerda@icloud.com>
2016-12-21 20:38:44 -0200

    Iniciando estrutura do site da casa do código.

---
+++ casadocodigo/app.js
@@ -0,0 +1,11 @@
+var express = require( 'express' );
+var app = express();
+
+
+app.get( '/produtos', function( req, res) {
+    res.send( "<html><body><h1> Listando os produtos </h1></body></html>" );    
+});
+
+app.listen( 3000, function(){
+    console.log( 'Servidor rodando' );
+});
