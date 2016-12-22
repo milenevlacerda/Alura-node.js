d287a520d8031f931dd8bf2964dbaf245175a7f7
Milene <milenelacerda@icloud.com>
2016-12-22 11:33:28 -0200

    Modificando app.js conforme aula 01

--- casadocodigo/app.js
+++ casadocodigo/app.js
@@ -1,9 +1,11 @@
 var express = require( 'express' );
 var app = express();
 
+app.set( 'view engine', 'ejs' );
 
 app.get( '/produtos', function( req, res) {
-    res.send( "<html><body><h1> Listando os produtos </h1></body></html>" );    
+    console.log( 'listando alterações' );
+    res.render( "produtos/lista" );
 });
 
 app.listen( 3000, function(){
