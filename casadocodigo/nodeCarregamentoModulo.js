d46655d8399038317cf1b201468108ada8a481bf
milenevlacerda <milenelacerda@icloud.com>
2016-12-23 19:04:41 -0200

    Adicionando arquivo de exemplo de carregamento de módulo

---
+++ casadocodigo/nodeCarregamentoModulo.js
@@ -0,0 +1,17 @@
+function require( path ) {
+    // ... Validations ...
+
+    var codigoDoSeuModulo = carregado( path );
+
+    var funcaoDeCarregamento = function() {
+        eval( codigoDoSeuModulo );
+    };
+
+    var module = {
+        exports: {}
+    };
+
+    funcaoDeCarregamento( module, module.exports );
+
+    return module;
+}
