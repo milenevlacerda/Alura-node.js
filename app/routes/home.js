9d12edaa454a1d39a601ba32857bd5f4043ef38a
milenevlacerda <milenelacerda@icloud.com>
2016-12-24 16:20:45 -0200

    Preparando deploy

---
+++ app/routes/home.js
@@ -0,0 +1,16 @@
+module.exports = function( app ) {
+    app.get( '/', function( req, res ) {
+
+        var connection = app.infra.connectionFactory();
+        var produtosDAO = new app.infra.ProdutosDAO( connection );
+
+        produtosDAO.lista( function( error, results ) {
+
+            res.render( 'home/index', { livros: results } );
+
+        });
+
+        connection.end();
+
+    });
+};
